d393619f7dff0f2644b1804b602f218ac221a150
Graham Watts <grahamcharleswatts@gmail.com>
2020-12-17 10:41:56 +0200

    Add problem details to DataCoreHttpClientException
    If an API call responds with an RFC 7807 problem details response describing an error, the deserialized object will be assigned to the `ProblemDetails` field on the `DataCoreHttpClientException` object.

--- src/IntelligentPlant.DataCore.HttpClient/DataCoreHttpClientException.cs
+++ src/IntelligentPlant.DataCore.HttpClient/DataCoreHttpClientException.cs
@@ -39,9 +39,16 @@ namespace IntelligentPlant.DataCore.Client {
         public IDictionary<string, string[]> ResponseHeaders { get; }
 
         /// <summary>
-        /// Gets the response content that was returned
+        /// Gets the response content that was returned.
         /// </summary>
         public string Content { get; }
+
+        /// <summary>
+        /// The RFC 7807 problem details object that was returned in the response body. This will 
+        /// be <see langword="null"/> unless the content type of the response was 
+        /// <see cref="ProblemDetails.Constants.MediaType"/>.
+        /// </summary>
+        public ProblemDetails.ProblemDetails ProblemDetails { get; }
 
 
         /// <summary>
@@ -68,8 +75,11 @@ namespace IntelligentPlant.DataCore.Client {
         /// <param name="responseContent">
         ///   The response content.
         /// </param>
-        public DataCoreHttpClientException(string message, string verb, string url, HttpStatusCode statusCode, IDictionary<string, string[]> requestHeaders, IDictionary<string, string[]> responseHeaders, string responseContent) 
-            : this(message, verb, url, statusCode, requestHeaders, responseHeaders, responseContent, null) { }
+        /// <param name="problemDetails">
+        ///   The RFC 7807 problem details object describing the error.
+        /// </param>
+        public DataCoreHttpClientException(string message, string verb, string url, HttpStatusCode statusCode, IDictionary<string, string[]> requestHeaders, IDictionary<string, string[]> responseHeaders, string responseContent, ProblemDetails.ProblemDetails problemDetails) 
+            : this(message, verb, url, statusCode, requestHeaders, responseHeaders, responseContent, problemDetails, null) { }
 
 
         /// <summary>
@@ -96,10 +106,13 @@ namespace IntelligentPlant.DataCore.Client {
         /// <param name="responseContent">
         ///   The response content.
         /// </param>
+        /// <param name="problemDetails">
+        ///   The RFC 7807 problem details object describing the error.
+        /// </param>
         /// <param name="inner">
         ///   The inner exception.
         /// </param>
-        public DataCoreHttpClientException(string message, string verb, string url, HttpStatusCode statusCode, IDictionary<string, string[]> requestHeaders, IDictionary<string, string[]> responseHeaders, string responseContent, Exception inner) 
+        public DataCoreHttpClientException(string message, string verb, string url, HttpStatusCode statusCode, IDictionary<string, string[]> requestHeaders, IDictionary<string, string[]> responseHeaders, string responseContent, ProblemDetails.ProblemDetails problemDetails, Exception inner) 
             : base(message, inner) {
             Verb = verb;
             Url = url;
@@ -107,31 +120,52 @@ namespace IntelligentPlant.DataCore.Client {
             RequestHeaders = new ReadOnlyDictionary<string, string[]>(requestHeaders ?? new Dictionary<string, string[]>());
             ResponseHeaders = new ReadOnlyDictionary<string, string[]>(responseHeaders ?? new Dictionary<string, string[]>());
             Content = responseContent;
+            ProblemDetails = problemDetails;
         }
 
 
         /// <summary>
-        /// Creates an <see cref="DataCoreHttpClientException"/> using the specified HTTP response message.
+        /// Creates an <see cref="DataCoreHttpClientException"/> using the specified HTTP response 
+        /// message.
         /// </summary>
-        /// <param name="errorMessage">The error message.</param>
-        /// <param name="response">The HTTP response.</param>
+        /// <param name="errorMessage">
+        ///   The error message.
+        /// </param>
+        /// <param name="response">
+        ///   The HTTP response.
+        /// </param>
         /// <returns>
         /// A task that will return the exception.
         /// </returns>
+        /// <remarks>
+        ///   If the response contains an RFC 7807 problem details object, the <see cref="ProblemDetails"/> 
+        ///   property of the exception will be set.
+        /// </remarks>
         internal static Task<DataCoreHttpClientException> FromHttpResponseMessage(string errorMessage, HttpResponseMessage response) {
             return FromHttpResponseMessage(errorMessage, response, null);
         }
 
 
         /// <summary>
-        /// Creates an <see cref="DataCoreHttpClientException"/> with an inner exception, using the specified HTTP response message.
+        /// Creates an <see cref="DataCoreHttpClientException"/> with an inner exception, using 
+        /// the specified HTTP response message.
         /// </summary>
-        /// <param name="errorMessage">The error message.</param>
-        /// <param name="response">The HTTP response.</param>
-        /// <param name="inner">The inner exception</param>
+        /// <param name="errorMessage">
+        ///   The error message.
+        /// </param>
+        /// <param name="response">
+        ///   The HTTP response.
+        /// </param>
+        /// <param name="inner">
+        ///   The inner exception.
+        /// </param>
         /// <returns>
         /// A task that will return the exception.
         /// </returns>
+        /// <remarks>
+        ///   If the response contains an RFC 7807 problem details object, the <see cref="ProblemDetails"/> 
+        ///   property of the exception will be set.
+        /// </remarks>
         internal static async Task<DataCoreHttpClientException> FromHttpResponseMessage(string errorMessage, HttpResponseMessage response, Exception inner) {
             if (response == null) {
                 throw new ArgumentNullException(nameof(response));
@@ -146,11 +180,15 @@ namespace IntelligentPlant.DataCore.Client {
                 : response.Headers.Concat(response.Content.Headers).ToDictionary(x => x.Key, x => x.Value.ToArray());
 
             var includeContent = responseHeaders.TryGetValue("Content-Type", out var contentTypes) && contentTypes != null
-                ? contentTypes.Any(x => CanIncludeContent(x))
+                ? contentTypes.Any(CanIncludeContent)
                 : false;
 
             var content = includeContent
                 ? await response.Content.ReadAsStringAsync().ConfigureAwait(false)
+                : null;
+
+            var problemDetails = includeContent && contentTypes.Any(IsProblemDetailsResponse)
+                ? Newtonsoft.Json.JsonConvert.DeserializeObject<ProblemDetails.ProblemDetails>(content)
                 : null;
 
             return new DataCoreHttpClientException(
@@ -161,8 +199,24 @@ namespace IntelligentPlant.DataCore.Client {
                 requestHeaders,
                 responseHeaders,
                 content, 
+                problemDetails,
                 inner
             );
+        }
+
+
+        /// <summary>
+        /// Tests if the specified content type represents an RFC 7807 response.
+        /// </summary>
+        /// <param name="contentType">
+        ///   The content type.
+        /// </param>
+        /// <returns>
+        ///   <see langword="true"/> if the content type is an RFC 7807 object, or <see langword="false"/> 
+        ///   otherwise.
+        /// </returns>
+        private static bool IsProblemDetailsResponse(string contentType) {
+            return contentType.StartsWith(IntelligentPlant.ProblemDetails.Constants.MediaType);
         }
 
 
@@ -180,6 +234,9 @@ namespace IntelligentPlant.DataCore.Client {
         private static bool CanIncludeContent(string contentType) {
             if (string.IsNullOrWhiteSpace(contentType)) {
                 return false;
+            }
+            if (IsProblemDetailsResponse(contentType)) {
+                return true;
             }
             if (contentType.StartsWith("application/json")) {
                 return true;
