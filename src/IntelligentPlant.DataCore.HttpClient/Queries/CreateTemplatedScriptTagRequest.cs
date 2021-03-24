e9bcdd0e821300bf833be89f311ff4ed6a30a741
Graham Watts <grahamcharleswatts@gmail.com>
2021-03-24 11:06:07 +0200

    Fix issue #6
    Fixes #6 by correcting the typo, but also adds a subclass with the original, misspelled name to maintain backwards compatibility.

--- src/IntelligentPlant.DataCore.HttpClient/Queries/CreateTemplatedScriptTagRequest.cs
+++ src/IntelligentPlant.DataCore.HttpClient/Queries/CreateTemplatedScriptTagRequest.cs
@@ -1,12 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 using IntelligentPlant.DataCore.Client.Model.Scripting.Templates;
 
 namespace IntelligentPlant.DataCore.Client.Queries {
-    public class CreateTemplatesScriptTagRequest : DataSourceRequest {
+    public class CreateTemplatedScriptTagRequest : DataSourceRequest {
 
         [Required]
         public CreateTemplatedScriptTagSettings Settings { get; set; }
 
     }
+
+
+    [Obsolete("Use " + nameof(CreateTemplatedScriptTagRequest) + " instead. This class name contains a typo and exists for backwards-compatibility but will be removed in the future.", false)]
+    public class CreateTemplatesScriptTagRequest : CreateTemplatedScriptTagRequest { }
 }
