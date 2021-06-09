a578cfd41f27e2ce56cfbec6a35f7c5caf9b2e19
Graham Watts <grahamcharleswatts@gmail.com>
2021-06-09 11:40:18 +0300

    Update ComponentName.cs
    Remove all non-default constructors

--- src/IntelligentPlant.DataCore.HttpClient/Model/ComponentName.cs
+++ src/IntelligentPlant.DataCore.HttpClient/Model/ComponentName.cs
@@ -41,45 +41,6 @@ namespace IntelligentPlant.DataCore.Client.Model {
 
 
         /// <summary>
-        /// Creates a new <see cref="ComponentName"/> using the specified <paramref name="name"/>, <paramref name="namespace"/> and <paramref name="displayName"/>.
-        /// </summary>
-        /// <param name="name">The component name.</param>
-        /// <param name="namespace">The component namespace.</param>
-        /// <param name="displayName">The display name.</param>
-        public ComponentName(string name, string @namespace, string displayName) {
-            Name = name;
-            Namespace = @namespace;
-            DisplayName = displayName;
-        }
-
-
-        /// <summary>
-        /// Creates a new <see cref="ComponentName"/> object.
-        /// </summary>
-        public ComponentName() : this(null, null, null) { }
-
-        /// <summary>
-        /// Creates a new <see cref="ComponentName"/> object from a single name string. Examine the name string syntax to determine if it is namespace qualified.
-        /// </summary>
-        /// <param name="name">The str</param>
-        public ComponentName(string name)
-        {
-            var nameParts = name.Split(new char['.'], 1);
-            if (nameParts.Length > 1)
-            {
-                Namespace = nameParts[0];
-                Name = nameParts[1];
-                DisplayName = nameParts[1];
-            }
-            else
-            {
-                Name = name;
-                DisplayName = name;
-            }
-        }
-
-
-        /// <summary>
         /// Gets the qualified name for the component with the specified name and namespace.
         /// </summary>
         /// <param name="name">The name.</param>
