f342a976ed0f25afa4fe2ed51c43c8fb36f49309
Graham Watts <grahamcharleswatts@gmail.com>
2024-10-23 12:45:54 +0300

    Add UtcCreatedAt property

--- src/IntelligentPlant.IndustrialAppStore.Authentication/OAuthTokens.cs
+++ src/IntelligentPlant.IndustrialAppStore.Authentication/OAuthTokens.cs
@@ -8,6 +8,11 @@ namespace IntelligentPlant.IndustrialAppStore.Authentication {
     public struct OAuthTokens {
 
         /// <summary>
+        /// The UTC time that the OAuth tokens were obtained at.
+        /// </summary>
+        public DateTimeOffset UtcCreatedAt { get; }
+
+        /// <summary>
         /// The type of the <see cref="AccessToken"/>.
         /// </summary>
         /// <remarks>
@@ -35,6 +40,9 @@ namespace IntelligentPlant.IndustrialAppStore.Authentication {
         /// <summary>
         /// Creates a new <see cref="OAuthTokens"/> instance.
         /// </summary>
+        /// <param name="utcCreatedAt">
+        ///   The UTC time that the OAuth tokens were obtained at.
+        /// </param>
         /// <param name="tokenType">
         ///   The type of the <paramref name="accessToken"/>. The token type is typically the string "bearer".
         /// </param>
@@ -50,7 +58,8 @@ namespace IntelligentPlant.IndustrialAppStore.Authentication {
         /// <exception cref="ArgumentNullException">
         ///   <paramref name="accessToken"/> is <see langword="null"/>.
         /// </exception>
-        public OAuthTokens(string tokenType, string accessToken, string? refreshToken, DateTimeOffset? utcExpiresAt) {
+        public OAuthTokens(DateTimeOffset utcCreatedAt, string tokenType, string accessToken, string? refreshToken, DateTimeOffset? utcExpiresAt) {
+            UtcCreatedAt = utcCreatedAt;
             TokenType = tokenType;
             AccessToken = accessToken ?? throw new ArgumentNullException(nameof(accessToken));
             RefreshToken = refreshToken;
