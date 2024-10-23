ca7bbc915bc0bd3dae632e1a9c1f0a559962915c
Graham Watts <grahamcharleswatts@gmail.com>
2024-10-23 12:47:53 +0300

    Renew cookie if token created at date has changed

--- src/IntelligentPlant.IndustrialAppStore.Authentication/IndustrialAppStoreAuthenticationExtensions.cs
+++ src/IntelligentPlant.IndustrialAppStore.Authentication/IndustrialAppStoreAuthenticationExtensions.cs
@@ -315,6 +315,11 @@ namespace Microsoft.Extensions.DependencyInjection {
                         var tokenStore = ctx.HttpContext.RequestServices.GetRequiredService<ITokenStore>();
                         await InitTokenStoreAsync(tokenStore, ctx.Principal!, ctx.Properties).ConfigureAwait(false);
 
+                        // The default cookie-based token store sets an authentication property
+                        // specifying when the access token was acquired. This will be null for
+                        // other token stores.
+                        var createdAtOriginal = ctx.Properties.GetTokenValue(IndustrialAppStoreAuthenticationDefaults.CreatedAtTokenName);
+
                         var accessToken = await tokenStore.GetTokensAsync().ConfigureAwait(false);
 
                         if (accessToken == null) {
@@ -322,6 +327,14 @@ namespace Microsoft.Extensions.DependencyInjection {
                             // will consider the cookie to be invalid.
                             ctx.RejectPrincipal();
                         }
+
+                        // If we are using the default cookie-based token store and the access
+                        // token was refreshed by the GetTokesAsync call above, the authentiation
+                        // property specifying the created-at timestamp will have changed.
+                        var createdAtUpdated = ctx.Properties.GetTokenValue(IndustrialAppStoreAuthenticationDefaults.CreatedAtTokenName);
+
+                        // If the access token has been refreshed, we need to renew the authentication cookie.
+                        ctx.ShouldRenew = !string.Equals(createdAtOriginal, createdAtUpdated, StringComparison.Ordinal);
 
                         if (cookieEvents.OnValidatePrincipal != null) {
                             await cookieEvents.OnValidatePrincipal(ctx).ConfigureAwait(false);
