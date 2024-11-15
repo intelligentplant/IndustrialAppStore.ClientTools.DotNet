fe5f29a3f42165385c2c691ce90702b778e8abf0
Graham Watts <grahamcharleswatts@gmail.com>
2024-11-15 09:21:26 +0200

    Use AccessTokenProvider to authenticate requests
    DefaultIndustrialAppStoreHttpFactory now creates HttpMessageHandler instances that authenticate outgoing requests using the AccessTokenProvider service.

--- src/IntelligentPlant.IndustrialAppStore.DependencyInjection/DefaultIndustrialAppStoreHttpFactory.cs
+++ src/IntelligentPlant.IndustrialAppStore.DependencyInjection/DefaultIndustrialAppStoreHttpFactory.cs
@@ -1,4 +1,6 @@
-﻿namespace IntelligentPlant.IndustrialAppStore.DependencyInjection {
+﻿using IntelligentPlant.IndustrialAppStore.DependencyInjection.Internal;
+
+namespace IntelligentPlant.IndustrialAppStore.DependencyInjection {
 
     /// <summary>
     /// Default implementation of <see cref="IIndustrialAppStoreHttpFactory"/>.
@@ -6,16 +8,36 @@
     public sealed class DefaultIndustrialAppStoreHttpFactory : IndustrialAppStoreHttpFactory {
 
         /// <summary>
+        /// The access token provider.
+        /// </summary>
+        private readonly AccessTokenProvider _accessTokenProvider;
+
+
+        /// <summary>
         /// Creates a new <see cref="DefaultIndustrialAppStoreHttpFactory"/> instance.
         /// </summary>
         /// <param name="httpMessageHandlerFactory">
         ///   The HTTP message handler factory.
         /// </param>
+        /// <param name="accessTokenProvider">
+        ///   The access token provider to use.
+        /// </param>
         /// <exception cref="System.ArgumentNullException">
         ///   <paramref name="httpMessageHandlerFactory"/> is <see langword="null"/>.
         /// </exception>
-        public DefaultIndustrialAppStoreHttpFactory(IHttpMessageHandlerFactory httpMessageHandlerFactory)
-            : base(httpMessageHandlerFactory) { }
+        public DefaultIndustrialAppStoreHttpFactory(IHttpMessageHandlerFactory httpMessageHandlerFactory, AccessTokenProvider accessTokenProvider)
+            : base(httpMessageHandlerFactory) {
+            _accessTokenProvider = accessTokenProvider ?? throw new ArgumentNullException(nameof(accessTokenProvider));
+        }
+
+
+        /// <inheritdoc/>
+        protected override HttpMessageHandler CreateHandler() {
+            var httpHandler = base.CreateHandler();
+            return Jaahas.Http.HttpClientFactory.CreatePipeline(
+                httpHandler, 
+                new AccessTokenAuthenticationHandler(_accessTokenProvider));
+        }
 
     }
 
